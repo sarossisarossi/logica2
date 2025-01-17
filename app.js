f32239f9fe2e5d16fc7c661017c51f895bd36ff1
Sergio Rossi <sarossi@gmail.com>
2025-01-17 16:52:36 -0300

    uso funcion document, link entre java y html

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del número secreto'
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'indica un numero del 1 al 10'
