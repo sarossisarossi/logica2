c1e9ffe88965ec621edf33da8e21cb4c1870774b
Sergio Rossi <sarossi@gmail.com>
2025-01-17 17:00:48 -0300

    primera funcion javascript

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ titulo.innerHTML = 'Juego del número secreto'
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'indica un numero del 1 al 10'
+
+function intentoDeUsuario(){
+
+    alert('click desde el boton')
+}
