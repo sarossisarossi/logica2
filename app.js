6f0d801ff9da7a3f0315cb4e5bebb1bc8b1cba11
Sergio Rossi <sarossi@gmail.com>
2025-01-20 14:28:46 -0300

    modificando funcion onclick en html y creando funciones y datatipe

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto)
 
-let titulo = document.querySelector('h1');
-    titulo.innerHTML = 'Juego del número secreto'
-
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'indica un numero del 1 al 10'
 
 
 function asignarTextoElemento(elemento,texto){
 
     let titulo = document.querySelector(elemento);
-    titulo.innerHTML = texto
+    titulo.innerHTML = texto;
 
 }
+
+function generarNumeroSecreto(){
+    
+    return Math.floor(Math.random()*10)+1;
+
+}
+
 
 asignarTextoElemento('h1','Juego del número secreto')
 asignarTextoElemento('p','indica un numero del 1 al 10')
 
-function intentoDeUsuario(){
+function verificarIntento(){
 
-    alert('click desde el boton')
+    let numeroDeUsuariostr = document.getElementById('valorUsuario').value;
+// numeroDeUsuariostr es un string, para que lo tome como numerico es la linea q vien a continuacion
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+    console.log(typeof(numeroDeUsuario ))
+    console.log(numeroDeUsuario);
+    console.log(numeroSecreto);
+    console.log(typeof(numeroSecreto ))
+// === ; es validacion en tipo de dato y valor == puede ser igual en valor pero un str con un int
+    console.log(numeroDeUsuario === numeroSecreto);
+    return numeroDeUsuario;
+
 }
 
 
