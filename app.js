9ec2e7d59471d1f0183afe97f0bb1e47c307ca5d
Sergio Rossi <sarossi@gmail.com>
2025-01-20 15:13:01 -0300

    agregamos boton y funcionalidades al juego

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let numeroSecreto = generarNumeroSecreto();
-console.log(numeroSecreto)
+let intentos = 1;
+
+condicionesIniciales();
+console.log(numeroSecreto);
 
 
 
@@ -16,24 +19,63 @@ function generarNumeroSecreto(){
 
 }
 
-
-asignarTextoElemento('h1','Juego del número secreto')
-asignarTextoElemento('p','indica un numero del 1 al 10')
+function condicionesIniciales(){
+    asignarTextoElemento('h1','Juego del número secreto');
+    asignarTextoElemento('p','indica un numero del 1 al 10');
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1 ;
+}
 
 function verificarIntento(){
-
+    console.log(intentos)
     let numeroDeUsuariostr = document.getElementById('valorUsuario').value;
 // numeroDeUsuariostr es un string, para que lo tome como numerico es la linea q vien a continuacion
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
 
-    console.log(typeof(numeroDeUsuario ))
-    console.log(numeroDeUsuario);
-    console.log(numeroSecreto);
-    console.log(typeof(numeroSecreto ))
-// === ; es validacion en tipo de dato y valor == puede ser igual en valor pero un str con un int
-    console.log(numeroDeUsuario === numeroSecreto);
+//     console.log(typeof(numeroDeUsuario ))
+//     console.log(numeroDeUsuario);
+//     console.log(numeroSecreto);
+//     console.log(typeof(numeroSecreto ))
+// // === ; es validacion en tipo de dato y valor == puede ser igual en valor pero un str con un int
+//     console.log(numeroDeUsuario === numeroSecreto);
+
+    if (numeroDeUsuario == numeroSecreto ){
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos ===1) ? 'vez' : 'veces'}`);
+        removerDisabled();
+    } else {
+        if ( numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'el número secreto es menor')
+        } 
+        else{
+            asignarTextoElemento('p', 'el número secreto es mayor')
+        }
+        intentos ++
+        limpiarCaja()
+    }
     return numeroDeUsuario;
 
 }
 
+function limpiarCaja(){
+    // let valorCaja = document.querySelector('input');
+    // alternativa colocar document.queryselector('#valorUsuario')   ; colocarle el # significa que busca por el id
+    let valorCaja = document.querySelector('#valorUsuario');
+    valorCaja.value ='';
+}
 
+function removerDisabled(){
+    document.getElementById('reiniciar').removeAttribute('disabled');
+}
+
+function reiniciarJuego(){
+    limpiarCaja();
+    // indicar mensaje de numeros
+
+    // generar numero aleatorio
+    
+    // boton desabilitar
+    // inicializar numero de intentos
+    condicionesIniciales();
+    document.querySelector('#reiniciar').setAttribute('disabled', true)
+    
+}
