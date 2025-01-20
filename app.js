432a23abca627cc6489d25c97e3e374234a8aa12
Sergio Rossi <sarossi@gmail.com>
2025-01-20 13:27:27 -0300

    creacion primera funcion

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+
 let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto'
+    titulo.innerHTML = 'Juego del número secreto'
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'indica un numero del 1 al 10'
+
+
+function asignarTextoElemento(elemento,texto){
+
+    let titulo = document.querySelector(elemento);
+    titulo.innerHTML = texto
+
+}
+
+asignarTextoElemento('h1','Juego del número secreto')
+asignarTextoElemento('p','indica un numero del 1 al 10')
 
 function intentoDeUsuario(){
 
     alert('click desde el boton')
 }
+
+
